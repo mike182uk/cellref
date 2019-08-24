2906e35d093be635a17c29c9ba21a46491c65c2d
Michael Barrett <mike182uk@gmail.com>
2019-08-24 10:05:48 +0100

    upgrade deps

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ const A1 = /^([A-Z]+)(\d+)$/
 /**
  * Auto detect notation used and convert to the opposite notation
  *
- * @param   {String} ref
- * @returns {String}
+ * @param   {string} ref
+ * @returns {string}
  * @throws  {Error}
  */
 
@@ -45,8 +45,8 @@ function cellref (ref) {
 /**
  * Convert A1 notation to R1C1 notation
  *
- * @param   {String} ref
- * @returns {String}
+ * @param   {string} ref
+ * @returns {string}
  * @throws  {Error}
  */
 
@@ -55,15 +55,15 @@ function convertA1toR1C1 (ref) {
     throw new Error(`${ref} is not a valid A1 cell reference`)
   }
 
-  var refParts = ref
+  const refParts = ref
     .replace(A1, '$1,$2')
     .split(',')
 
-  var columnStr = refParts[0]
-  var row = refParts[1]
-  var column = 0
+  const columnStr = refParts[0]
+  const row = refParts[1]
+  let column = 0
 
-  for (var i = 0; i < columnStr.length; i++) {
+  for (let i = 0; i < columnStr.length; i++) {
     column = 26 * column + columnStr.charCodeAt(i) - 64
   }
 
@@ -73,8 +73,8 @@ function convertA1toR1C1 (ref) {
 /**
  * Convert R1C1 notation to A1 notation
  *
- * @param {String} ref
- * @returns {String}
+ * @param {string} ref
+ * @returns {string}
  * @throws {Error}
  */
 
@@ -83,13 +83,13 @@ function convertR1C1toA1 (ref) {
     throw new Error(`${ref} is not a valid R1C1 cell reference`)
   }
 
-  var refParts = ref
+  const refParts = ref
     .replace(R1C1, '$1,$2')
     .split(',')
 
-  var row = refParts[0]
-  var column = refParts[1]
-  var columnStr = ''
+  const row = refParts[0]
+  let column = refParts[1]
+  let columnStr = ''
 
   for (; column; column = Math.floor((column - 1) / 26)) {
     columnStr = String.fromCharCode(((column - 1) % 26) + 65) + columnStr
